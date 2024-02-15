90994ba77d1c82987059ce08705d35aa6aa6b77b
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-15 13:57:57 -0600

    finished prob 2- Mohamad ali fakhoury

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -8,7 +8,24 @@ Lab Time: Thurs @2pm
 def password_mod():
     word = input()
     password = ''
-    
+    x = len(word)
+    while len(word) - x < len(word):
+        if word[len(word)-x] == 'm':
+            password = password + 'M'
+        elif word[len(word)-x] == 'i':
+           password = password + '1'
+        elif word[len(word)-x] == 'a':
+           password = password + '@'
+        elif word[len(word)-x] == 'B':
+           password = password + '8'
+        elif word[len(word)-x] == 's':
+           password = password + '$'
+        else:
+           password = password + word[len(word)-x]
+        x= x - 1
+    else: 
+     password = password + '!'
+     print(password)
 
 if __name__ == "__main__":
     password_mod()
