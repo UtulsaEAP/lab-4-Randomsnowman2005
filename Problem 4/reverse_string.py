7b1b919b7711cbcb199728359835df8f21d50841
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-15 15:05:59 -0600

    finished prob 5 - Mohamad ali fakhoury

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -6,8 +6,35 @@ Lab Time: Thurs @2pm
 """
 
 def reverse_string():
-    tom = input()
-    
+    x = 0
+    inputs = []
+    inp1 = ''
+    z=0
+    outputs = []
+    while z<1 :
+        inp1 = str(input())
+        if inp1 =="done" or inp1 =="d" or inp1 == "Done":
+         z = z+1
+        else: 
+         inputs.insert(x,inp1)
+         x = x + 1
+    else:  
+        x=x-1
+        z = 0
+        while x >=0:
+         z = len(inputs[x])
+         final = ''
+         while z> 0:
+            word = inputs[x]
+            let = word[z-1]
+            final = final + let
+            z = z -1
+         outputs.insert(x,final)
+         x=x-1
+    x= len(outputs) - 1
+    while x>=0:
+     print (outputs[x])
+     x= x - 1   
 
 if __name__ == "__main__":
     reverse_string()
