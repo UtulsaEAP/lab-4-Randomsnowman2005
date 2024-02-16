f3ecb3012e7cb96e73f6991e04438be374adb066
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-15 21:42:24 -0600

    prob 6 done, that was fun- Mohamad ali fakhoury

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -6,7 +6,21 @@ Lab Time: Thurs @ 2pm
 """
 
 def norm():
-    tom = input()
+    nran = int(input())
+    i = 0
+    ran =[]
+    while i < nran:
+        ran.insert(i,float(input()))
+        i = i + 1
+    i=0
+    for i in range(0, nran-1):
+        if ran[i] > ran[i+1]:
+            div = ran[i]
+        if ran[nran-1] > ran[i]:
+            div = ran[nran-1]
+    for i in range(0, nran):   
+        print(f'{ran[i]/div:.2f}') 
 
+    
 if __name__ == "__main__":
     norm()
